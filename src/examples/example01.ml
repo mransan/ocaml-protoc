923f98e1708249f392757fe1d646702528faf652
mransan <maxime.ransan@gmail.com>
2017-03-15 09:45:08 -0400

    Remove files which are now in ocaml-protoc-yojson

--- src/examples/example01.ml
+++ src/examples/example01.ml
@@ -1,29 +1,25 @@
-
-let person = Example01_pb.({ 
-  first_name = "John Doe"; 
-  id = 1234l;
-  email = "jdoe@example.com"; 
-  child = Some {cute_name = "Booboo"}; 
-  second_child = None;
-  phone = ["917-929-8071"; "646-269-2829"];
-  residence = House 12.;
-  gender = Female;
-}) 
-
-(*
 let () = 
+  (* Create OCaml value of generated type *) 
+
+  let person = Example01_pb.({ 
+    name = "John Doe"; 
+    id = 1234l;
+    email = Some "jdoe@example.com"; 
+    phone = ["123-456-7890"];
+  }) in 
+  
   (* Create a Protobuf encoder and encode value *)
 
   let encoder = Pbrt.Encoder.create () in 
   Example01_pb.encode_person person encoder; 
 
   (* Output the protobuf message to a file *) 
+
   let oc = open_out "myfile" in 
   output_bytes oc (Pbrt.Encoder.to_bytes encoder);
   close_out oc
-  *)
 
-(*let () = 
+let () = 
   
   let bytes = 
     let ic = open_in "myfile" in 
@@ -35,21 +31,4 @@ let () =
   in 
   
   let person = Example01_pb.decode_person (Pbrt.Decoder.of_bytes bytes) in 
-  Format.fprintf Format.std_formatter "debug:\n%a\n" 
-                 Example01_pb.pp_person person *)
-
-module JsonEncoder = Example01_pb.Make_encoder(Pbrt_js_yojson.Encoder) 
-module JsonDecoder = Example01_pb.Make_decoder(Pbrt_js_yojson.Decoder)
-
-let () = 
-  let encoder = Pbrt_js_yojson.Encoder.empty () in 
-  JsonEncoder.encode_person person encoder; 
-  let json_str = Yojson.Basic.to_string (`Assoc !encoder) in 
-  print_endline "Json value:"; 
-  print_endline json_str; 
-  match Yojson.Basic.from_string json_str with 
-  | `Assoc a -> 
-     let decoder = ref a in
-     let person' = JsonDecoder.decode_person decoder in 
-     assert(person' = person)
-  | _ -> assert(false)
+  Format.fprintf Format.std_formatter "%a" Example01_pb.pp_person person 
