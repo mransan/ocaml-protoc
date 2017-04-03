3eb742d68059be2179dc6aaddc42db325e3d9691
mransan <maxime.ransan@gmail.com>
2017-04-02 17:08:49 -0700

    Split ocaml_protoc into separate module for cmdline parsing/compilation and code generation

---
+++ src/ocaml-protoc/ocaml_protoc_compilation.ml
@@ -0,0 +1,106 @@
+(*
+  The MIT License (MIT)
+  
+  Copyright (c) 2016 Maxime Ransan <maxime.ransan@gmail.com>
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*)
+
+module E = Pb_exception
+module Loc = Pb_location
+module Pt = Pb_parsing_parse_tree 
+module Parsing_util = Pb_parsing_util
+module Tt = Pb_typing_type_tree 
+module Typing_util = Pb_typing_util
+module Ot = Pb_codegen_ocaml_type 
+module Cmdline = Ocaml_protoc_cmdline.Cmdline
+
+let find_imported_file include_dirs file_name = 
+  if Sys.file_exists file_name
+  then file_name
+  else 
+    let found_file = List.fold_left (fun found_file include_dir -> 
+      let try_file_name = Filename.concat include_dir file_name in 
+      match found_file, Sys.file_exists try_file_name with 
+      | None , true  -> Some try_file_name 
+      | Some previous, true  -> (
+        Printf.eprintf 
+            ("[Warning] Imported file %s found in 2 directories, " ^^ 
+             "picking: %s\n")
+            file_name previous; 
+        found_file
+      )
+      | _, false -> found_file  
+    ) None include_dirs in
+
+    match found_file with
+    | None -> E.import_file_not_found file_name 
+    | Some file_name -> file_name 
+
+let compile cmdline cmd_line_files_options = 
+
+  let {
+    Cmdline.include_dirs; 
+    Cmdline.proto_file_name; _} = cmdline in
+
+  (* parsing *) 
+
+  let protos = Pb_parsing.parse_file (fun file_name -> 
+    let file_name = find_imported_file include_dirs file_name in 
+    (file_name, Pb_util.read_file file_name)  
+  ) proto_file_name in 
+
+  (* file options can be overriden/added with command line arguments *)
+  let protos = List.map (fun proto -> 
+    {proto with 
+      Pt.file_options = 
+        Pb_option.merge proto.Pt.file_options cmd_line_files_options 
+    }
+  ) protos in
+
+  let proto_file_options = 
+    let main_proto = List.hd protos in 
+    main_proto.Pt.file_options
+  in 
+
+  (* typing *) 
+
+  let grouped_protos = Pb_typing.perform_typing protos in 
+  let all_typed_protos = List.flatten grouped_protos in 
+
+  (* Only get the types which are part of the given proto file 
+   * (compilation unit) *)
+  let grouped_proto = List.filter (function
+    | {Tt.file_name; _ }::_ when file_name = proto_file_name -> true 
+    | _ -> false
+  ) grouped_protos in 
+
+  (* -- OCaml Backend -- *)
+
+  let module BO = Pb_codegen_backend in 
+
+  let ocaml_types = List.rev @@ List.fold_left (fun ocaml_types types -> 
+    let l = List.flatten @@ List.map (fun t -> 
+      BO.compile all_typed_protos t
+    ) types in 
+    l :: ocaml_types 
+  ) [] grouped_proto  in 
+
+  (ocaml_types, proto_file_options)
---
+++ src/ocaml-protoc/ocaml_protoc_generation.ml
@@ -0,0 +1,269 @@
+(*
+  The MIT License (MIT)
+  
+  Copyright (c) 2016 Maxime Ransan <maxime.ransan@gmail.com>
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*)
+
+module E = Pb_exception
+module Ot = Pb_codegen_ocaml_type 
+module F = Pb_codegen_formatting
+module Codegen_util = Pb_codegen_util 
+module Cmdline = Ocaml_protoc_cmdline.Cmdline
+
+type codegen_f = ?and_:unit -> Ot.type_ -> F.scope -> bool 
+
+let generate_for_all_types ocaml_types sc (f:codegen_f) ocamldoc_title = 
+  begin
+    match ocamldoc_title with
+    | None -> () 
+    | Some ocamldoc_title -> ( 
+      F.empty_line sc;
+      F.linep sc "(** {2 %s} *)" ocamldoc_title;  
+      F.empty_line sc;
+    )
+  end;
+
+  List.iter (fun types -> 
+    let _:bool = List.fold_left (fun first type_ -> 
+      let has_encoded = if first 
+        then f type_ sc 
+        else f ~and_:() type_ sc
+      in 
+      F.empty_line sc;
+      first && (not has_encoded) 
+    ) true types in 
+    ()
+  ) ocaml_types 
+
+let open_files cmdline file_suffix = 
+  let {
+    Cmdline.ml_out; 
+    Cmdline.proto_file_name; _} = cmdline in 
+
+  let out_file_name = 
+    let proto_file_name = Filename.basename proto_file_name in
+    let out_basename = 
+      Codegen_util.caml_file_name_of_proto_file_name 
+        ~proto_file_name ~file_suffix
+    in 
+    Filename.concat ml_out out_basename
+  in 
+  
+  Printf.printf "Generating %s.mli\n%!" out_file_name; 
+  Printf.printf "Generating %s.ml\n%!"  out_file_name; 
+
+  (open_out @@ out_file_name ^ ".mli" , open_out @@ out_file_name ^ ".ml")
+
+let generate_type_and_default 
+       ocaml_types proto_file_options cmdline = 
+
+  let file_suffix = "types" in 
+
+  let print_ppx sc = 
+    match Pb_option.get proto_file_options "ocaml_file_ppx" with
+    | None -> () 
+    | Some Pb_option.Constant_string s -> 
+      F.linep sc "[@@@%s]" s
+    | _ -> E.invalid_ppx_extension_option cmdline.Cmdline.proto_file_name  
+  in 
+
+  (* .ml file *) 
+  let ml_sc = F.empty_scope () in 
+  F.line ml_sc "[@@@ocaml.warning \"-27-30-39\"]";
+  F.empty_line ml_sc; 
+  print_ppx ml_sc; 
+  F.empty_line ml_sc;
+  generate_for_all_types ocaml_types ml_sc Pb_codegen_types.gen_struct None;
+  generate_for_all_types ocaml_types ml_sc Pb_codegen_default.gen_struct None;
+
+  (* .mli file *)
+  let mli_sc = F.empty_scope () in 
+  F.linep mli_sc "(** %s Types *)" 
+    (Filename.basename cmdline.Cmdline.proto_file_name); 
+  F.empty_line mli_sc; 
+  print_ppx mli_sc; 
+  F.empty_line mli_sc;
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_types.gen_sig 
+    (Some Pb_codegen_types.ocamldoc_title);
+  generate_for_all_types ocaml_types mli_sc
+    Pb_codegen_default.gen_sig 
+    (Some Pb_codegen_default.ocamldoc_title);
+
+  let sig_oc, struct_oc = open_files cmdline file_suffix in 
+  output_string struct_oc (F.print ml_sc);
+  output_string sig_oc (F.print mli_sc)
+
+let generate_mutable_records ocaml_types sc = 
+  let ocaml_types = List.flatten ocaml_types in 
+  List.iter (fun {Ot.spec; module_; _ } -> 
+    match spec with
+    | Ot.Record r -> 
+      Pb_codegen_types.gen_record ~mutable_:() module_ r sc;
+      F.empty_line sc;
+      Pb_codegen_default.gen_record ~mutable_:() module_ r sc; 
+      F.empty_line sc;
+    | _ -> () 
+  ) ocaml_types
+
+let generate_yojson ocaml_types cmdline = 
+  let file_suffix = "yojson" in 
+
+  (* .ml file *) 
+  let ml_sc = F.empty_scope () in 
+  F.line ml_sc "[@@@ocaml.warning \"-27-30-39\"]";
+  F.empty_line ml_sc;
+  generate_mutable_records ocaml_types ml_sc;
+  F.empty_line ml_sc;
+
+  generate_for_all_types ocaml_types ml_sc 
+    Pb_codegen_decode_yojson.gen_struct None; 
+  generate_for_all_types ocaml_types ml_sc 
+    Pb_codegen_encode_yojson.gen_struct None; 
+  
+  (* .mli file *)
+  let mli_sc = F.empty_scope () in 
+  F.linep mli_sc 
+        "(** %s YoJSON Encoding *)" 
+        (Filename.basename cmdline.Cmdline.proto_file_name); 
+  F.empty_line mli_sc; 
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_encode_yojson.gen_sig 
+    (Some Pb_codegen_encode_yojson.ocamldoc_title);
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_decode_yojson.gen_sig 
+    (Some Pb_codegen_decode_yojson.ocamldoc_title);
+
+  let sig_oc, struct_oc = open_files cmdline file_suffix in 
+  output_string struct_oc (F.print ml_sc);
+  output_string sig_oc (F.print mli_sc)
+
+let generate_bs ocaml_types cmdline = 
+  let file_suffix = "bs" in 
+
+  (* .ml file *) 
+  let ml_sc = F.empty_scope () in 
+  F.line ml_sc "[@@@ocaml.warning \"-27-30-39\"]";
+  F.empty_line ml_sc;
+  generate_mutable_records ocaml_types ml_sc;
+  F.empty_line ml_sc;
+
+  generate_for_all_types ocaml_types ml_sc 
+    Pb_codegen_decode_bs.gen_struct None; 
+  generate_for_all_types ocaml_types ml_sc 
+    Pb_codegen_encode_bs.gen_struct None; 
+  
+  (* .mli file *)
+  let mli_sc = F.empty_scope () in 
+  F.linep mli_sc 
+        "(** %s BuckleScript Encoding *)" 
+        (Filename.basename cmdline.Cmdline.proto_file_name); 
+  F.empty_line mli_sc; 
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_encode_bs.gen_sig 
+    (Some Pb_codegen_encode_bs.ocamldoc_title);
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_decode_bs.gen_sig 
+    (Some Pb_codegen_decode_bs.ocamldoc_title);
+  
+  let sig_oc, struct_oc = open_files cmdline file_suffix in 
+  output_string struct_oc (F.print ml_sc);
+  output_string sig_oc (F.print mli_sc)
+
+let generate_binary ocaml_types cmdline =
+  let file_suffix = "pb" in 
+
+  (* .ml file *) 
+  let ml_sc = F.empty_scope () in 
+  F.line ml_sc "[@@@ocaml.warning \"-27-30-39\"]";
+  F.empty_line ml_sc;
+  generate_mutable_records ocaml_types ml_sc;
+  F.empty_line ml_sc;
+
+  generate_for_all_types ocaml_types ml_sc 
+    Pb_codegen_decode_binary.gen_struct None; 
+  generate_for_all_types ocaml_types ml_sc 
+    Pb_codegen_encode_binary.gen_struct None; 
+  
+  (* .mli file *)
+  let mli_sc = F.empty_scope () in 
+  F.linep mli_sc 
+        "(** %s Binary Encoding *)" 
+        (Filename.basename cmdline.Cmdline.proto_file_name); 
+  F.empty_line mli_sc; 
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_encode_binary.gen_sig 
+    (Some Pb_codegen_encode_binary.ocamldoc_title);
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_decode_binary.gen_sig 
+    (Some Pb_codegen_decode_binary.ocamldoc_title);
+
+  let sig_oc, struct_oc = open_files cmdline file_suffix in 
+  output_string struct_oc (F.print ml_sc);
+  output_string sig_oc (F.print mli_sc)
+
+let generate_pp ocaml_types cmdline =
+  let file_suffix = "pp" in 
+
+  (* .ml file *) 
+  let ml_sc = F.empty_scope () in 
+  F.line ml_sc "[@@@ocaml.warning \"-27-30-39\"]";
+  F.empty_line ml_sc;
+
+  generate_for_all_types ocaml_types ml_sc 
+    Pb_codegen_pp.gen_struct None; 
+  
+  (* .mli file *)
+  let mli_sc = F.empty_scope () in 
+  F.linep mli_sc 
+    "(** %s Pretty Printing *)" 
+    (Filename.basename cmdline.Cmdline.proto_file_name); 
+  F.empty_line mli_sc; 
+  generate_for_all_types ocaml_types mli_sc 
+    Pb_codegen_pp.gen_sig 
+    (Some Pb_codegen_pp.ocamldoc_title);
+
+  let sig_oc, struct_oc = open_files cmdline file_suffix in 
+  output_string struct_oc (F.print ml_sc);
+  output_string sig_oc (F.print mli_sc)
+
+let generate_code ocaml_types proto_file_options cmdline = 
+
+  generate_type_and_default 
+    ocaml_types proto_file_options cmdline;
+
+  begin if !(cmdline.Cmdline.yojson)  
+  then generate_yojson ocaml_types cmdline 
+  end; 
+  
+  begin if !(cmdline.Cmdline.binary)
+  then generate_binary ocaml_types cmdline
+  end; 
+  
+  begin if !(cmdline.Cmdline.pp)
+  then generate_pp ocaml_types cmdline
+  end; 
+  
+  begin if !(cmdline.Cmdline.bs)
+  then generate_bs ocaml_types cmdline
+  end; 
+  ()
