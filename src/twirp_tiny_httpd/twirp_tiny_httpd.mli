8addb046699015636b999ffb469df70b568fb79d
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-09-11 23:58:31 -0400

    doc

--- src/twirp_tiny_httpd/twirp_tiny_httpd.mli
+++ src/twirp_tiny_httpd/twirp_tiny_httpd.mli
@@ -14,5 +14,14 @@ val add_service :
 (** [add_service http_server service] adds all handlers of [service]
     to the given httpd.
 
-    Services that use streaming will return "501 unimplemented", because
-    twirp over http 1.1 does not support streaming. *)
+    As a reminder, a RPC "foo" under service "bar" is routed under [prefix/bar/foo]
+    using the POST method.
+    The body is either binary, alongside the header "application/protobuf",
+    or JSON, alongside the header "application/json".
+    Errors are always encoded as a JSON object and are indicated by a
+    failing HTTP code (outside of [200…299] range).
+
+    RPCs that use streaming (on either the client or server side)
+    will always return "501 unimplemented", because
+    twirp over http 1.1 does not support streaming.
+*)
