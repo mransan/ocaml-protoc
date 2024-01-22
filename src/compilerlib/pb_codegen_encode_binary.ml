6187a6b8e371287b250d7322c40a21a5f760422a
Konstantin Olkhovskiy <lupus@oxnull.net>
2024-01-22 16:49:32 +0400

    Expose one-of per-constructor options

--- src/compilerlib/pb_codegen_encode_binary.ml
+++ src/compilerlib/pb_codegen_encode_binary.ml
@@ -139,6 +139,7 @@ let gen_rft_variant sc var_name { Ot.v_constructors; _ } =
         vc_field_type;
         vc_encoding_number;
         vc_payload_kind;
+        vc_options = _;
       } =
         constructor
       in
@@ -235,6 +236,7 @@ let gen_variant ?and_ variant sc =
             vc_field_type;
             vc_encoding_number;
             vc_payload_kind;
+            vc_options = _;
           } =
             variant_constructor
           in
