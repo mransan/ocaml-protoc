38e6a15ab21ec19a35e1e982f190ce29b6930e81
Konstantin Olkhovskiy <lupus@oxnull.net>
2024-03-08 09:00:54 +0400

    String.starts_with...

--- src/compilerlib/pb_util.ml
+++ src/compilerlib/pb_util.ml
@@ -47,6 +47,22 @@ let string_fold_lefti f e0 s =
   in
   loop e0 0
 
+module String = struct
+  include String
+
+  let starts_with ~prefix s =
+    let len_s = length s and len_pre = length prefix in
+    let rec aux i =
+      if i = len_pre then
+        true
+      else if unsafe_get s i <> unsafe_get prefix i then
+        false
+      else
+        aux (i + 1)
+    in
+    len_s >= len_pre && aux 0
+end
+
 module Option = struct
   let default x = function
     | Some y -> y
