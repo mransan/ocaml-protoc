ae2faa3723958c92508d2424102c982b1fe789aa
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-11-03 13:39:06 -0500

    update tests

--- src/examples/t_example04.ml
+++ src/examples/t_example04.ml
@@ -1,6 +1,6 @@
 open Example04
 
-let ( @+ ) value next = Cons { value; next = Some next }
+let ( @+ ) value next = Cons (make_int_list_cons ~value ~next ())
 
 let () =
   let l = 1 @+ 2 @+ 3 @+ 4 @+ 5 @+ Nil in
