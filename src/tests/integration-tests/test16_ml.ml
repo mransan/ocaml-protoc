7157843a8201e226c737c2158e50ae3226b96316
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-11-04 11:15:21 -0500

    update test outputs

--- src/tests/integration-tests/test16_ml.ml
+++ src/tests/integration-tests/test16_ml.ml
@@ -7,8 +7,8 @@ let of_list e0 l =
 
 let decode_pb_ref_data () =
   let v = T.make_m ~f1:1l ~f2:2l ~f3:3l ~f4:(of_list 0l [ 1l; 2l ]) () in
-  T.set_m_f3 v 4l;
-  T.set_m_f4 v @@ of_list 0l [ 3l; 4l ];
+  T.m_set_f3 v 4l;
+  T.m_set_f4 v @@ of_list 0l [ 3l; 4l ];
   v
 
 let mode = Test_util.parse_args ()
