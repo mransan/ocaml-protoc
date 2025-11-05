b6cba1606ca20788ff763bd834955f0d07e91e93
Simon Cruanes <simon.cruanes.2007@m4x.org>
2025-11-05 17:42:29 -0500

    format

--- src/runtime-pb-options/pbrt_pb_options.mli
+++ src/runtime-pb-options/pbrt_pb_options.mli
@@ -1,7 +1,7 @@
 (** Protobuf JSON encoding runtime *)
 
-(** All exception which could be raised by the generated JSON encoder
-    and decode function *)
+(** All exception which could be raised by the generated JSON encoder and decode
+    function *)
 module E : sig
   type error =
     | Unexpected_option_type of string * string
@@ -11,18 +11,17 @@ module E : sig
   (** Decoding/Encoding failure *)
 
   val unexpected_option_type : string -> string -> 'a
-  (** [unexpected_option_type record_name field_name] raises 
+  (** [unexpected_option_type record_name field_name] raises
       [Failure (Unexpected_json_type (record_name, field_name))] *)
 
   val malformed_variant : string -> 'a
-  (** [malformed_variant variant_name] raise 
+  (** [malformed_variant variant_name] raise
       [Failure (Malformed_variant variant_name)] *)
 end
 
 open Ocaml_protoc_compiler_lib
 
-(** Helper module for the generated code for common 
-    functionality *)
+(** Helper module for the generated code for common functionality *)
 
 val string : Pb_option.value -> string -> string -> string
 val float : Pb_option.value -> string -> string -> float
